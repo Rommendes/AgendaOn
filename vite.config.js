6c2392a858e5a5a848127ca0eabbee0d95e2cd8d
Rosangela <96451367+Rommendes@users.noreply.github.com>
2025-11-03 13:51:32 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 
-
+{/**
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -11,4 +11,23 @@ export default defineConfig({
       '/api': 'http://localhost:3001'
     }
   }
+})
+ */}
+
+ import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+// Configuração principal do Vite
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'), // agora "@" aponta para a pasta src/
+    },
+  },
+  server: {
+    port: 5173, // porta padrão do Vite
+    open: true, // abre o navegador automaticamente
+  },
 })
